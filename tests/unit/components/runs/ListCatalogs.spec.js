4f07e35efc8fd7ea67421db069fa9ede0856169e
Joshua Avery <joshuaaver01@gmail.com>
2020-07-30 14:39:05 -0700

    Finished testing for ListCatalogs

--- tests/unit/components/runs/ListCatalogs.spec.js
+++ tests/unit/components/runs/ListCatalogs.spec.js
@@ -33,26 +33,26 @@ describe('Catalogs component', () => {
       {
         localVue,
         router,
-        mocks: {
-          $store: {
-            commit: jest.fn(),
-            dispatch: jest.fn(),
-            getters:
-              {
-                'login/user': 'Sauron',
-              },
-          },
-        },
       });
+    expect(wrapper.findComponent(bootstrap.BListGroup).exists()).toBe(false);
+    expect(wrapper.find('h1').exists()).toBe(false);
+    expect(wrapper.find('h3').exists()).toBe(false);
+    expect(wrapper.find('p').exists()).toBe(false);
     await wrapper.vm.$nextTick();
     await wrapper.vm.$nextTick();
-
+    expect(wrapper.find('p').exists()).toBe(false);
     const getMock = localVue.prototype.$api.get;
     expect(getMock).toHaveBeenCalledTimes(1);
     expect(getMock).lastCalledWith(wrapper.vm.$runs_url);
 
+    const title = wrapper.find('h1');
+    expect(title.exists()).toBe(true);
+    expect(title.text()).toBe('Catalogs:');
+    expect(wrapper.find('h3').exists()).toBe(false);
+
     const listGroup = wrapper.findComponent(bootstrap.BListGroup);
     expect(listGroup.exists()).toBe(true);
+    expect(listGroup.element).toBeVisible();
     const catalogNames = responses.catalogsResp.catalogs;
     const catalogs = wrapper.findAllComponents(bootstrap.BListGroupItem);
     expect(catalogs.length).toBe(catalogNames.length);
@@ -61,5 +61,39 @@ describe('Catalogs component', () => {
       expect(item.text()).toBe(name);
       expect(item.attributes('href')).toBe(name);
     });
+  });
+
+  it('notifies the user if there are not catalogs', async () => {
+    localVue.prototype.$api.get.mockResolvedValue({ data: { catalogs: [] } });
+    const wrapper = mount(ListCatalogs,
+      {
+        localVue,
+        router,
+      });
+    await wrapper.vm.$nextTick();
+    await wrapper.vm.$nextTick();
+    expect(wrapper.findAllComponents(bootstrap.BListGroupItem).length).toBe(0);
+    const p = wrapper.find('p');
+    expect(p.exists()).toBe(true);
+    expect(p.element).toBeVisible();
+    expect(p.text()).toBe('0 catalogs found');
+  });
+
+  it('on an error, notifies the user', async () => {
+    localVue.prototype.$api.get.mockRejectedValue(new Error('Cast it into the fire! Destroy it!'));
+    const wrapper = mount(ListCatalogs,
+      {
+        localVue,
+        router,
+      });
+    await wrapper.vm.$nextTick();
+    await wrapper.vm.$nextTick();
+    expect(wrapper.findComponent(bootstrap.BListGroup).exists()).toBe(false);
+    expect(wrapper.find('h1').exists()).toBe(false);
+    expect(wrapper.find('p').exists()).toBe(false);
+    const h3 = wrapper.find('h3');
+    expect(h3.exists()).toBe(true);
+    expect(h3.element).toBeVisible();
+    expect(h3.text()).toBe('Something went wrong. Try reloading the page. If the problem persists contact an administrator');
   });
 });
