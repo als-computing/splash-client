6aca75f68c7490c3c235d7f26cff3c11e605587b
thefearlesscoder <34893923+thefearlesscoder@users.noreply.github.com>
2020-09-11 17:30:12 -0700

    compounds interface can now handle empty arrays

--- src/views/Compound.vue
+++ src/views/Compound.vue
@@ -10,62 +10,69 @@
           <b-col lg="9">
             <b-card>
               <div align="left">
-              <b-button
+                <b-button
                   @click="addSection(0)"
                   :disabled="currently_edited_index !== undefined"
                 >Add section</b-button>
-                </div>
-              <div
-                align="left"
-                v-for="(section, index) in compound.documentation.sections"
-                :key="index + section.title + section.text"
-              >
-                <div @dblclick="edit(index, section.text, section.title)">
-                  <div v-show="index !== currently_edited_index">
-                    <p>
-                      <strong>{{section.title}}</strong>
-                      <span class="pointer" @click="edit(index, section.text, section.title)">
-                        <u>[edit]</u>
-                      </span>
-                      <span class="text-muted" style="font-size:0.8rem">(or double click)</span>
-                    </p>
-                    <div class="markdown-html" v-html="parseMarkdown(section.text)" />
+              </div>
+              <h4 v-if="compound.documentation.sections.length == 0">No documentation for this compound. Be the first to add some!</h4>
+              <div v-if="compound.documentation.sections.length !== 0">
+                <div
+                  align="left"
+                  v-for="(section, index) in compound.documentation.sections"
+                  :key="index + section.title + section.text"
+                >
+                  <div @dblclick="edit(index, section.text, section.title)">
+                    <div v-show="index !== currently_edited_index">
+                      <p>
+                        <strong>{{section.title}}</strong>
+                        <span class="pointer" @click="edit(index, section.text, section.title)">
+                          <u>[edit]</u>
+                        </span>
+                        <span class="text-muted" style="font-size:0.8rem">(or double click)</span>
+                      </p>
+                      <div class="markdown-html" v-html="parseMarkdown(section.text)" />
+                    </div>
+                    <div v-if="index === currently_edited_index">
+                      <b-form-input v-model="edited_data.title" :readonly="saving" />
+                      <b-form-textarea
+                        v-model="edited_data.text"
+                        max-rows="100"
+                        :readonly="saving"
+                      />
+                      <b-button-toolbar>
+                        <b-button
+                          variant="primary"
+                          @click="saveEdit(index, section)"
+                          :disabled=" (edited_data.title === '' || edited_data.text === '') || (((edited_data.title === section.title) && (edited_data.text === section.text)) || saving)"
+                        >Save</b-button>
+                        <b-button
+                          variant="danger"
+                          @click="removeFocus(); deleteIfNew(index, section)"
+                          :disabled="saving === true"
+                        >Cancel</b-button>
+                        <b-button
+                          variant="warning"
+                          @click="showDeleteConfirmation(index)"
+                          v-show="!(section.title === '' || section.text === '') && !saving"
+                        >Delete Section</b-button>
+                        <b-spinner v-show="(saving === true)" />
+                      </b-button-toolbar>
+                    </div>
                   </div>
-                  <div v-if="index === currently_edited_index">
-                    <b-form-input v-model="edited_data.title" :readonly="saving" />
-                    <b-form-textarea v-model="edited_data.text" max-rows="100" :readonly="saving" />
-                    <b-button-toolbar>
-                      <b-button
-                        variant="primary"
-                        @click="saveEdit(index, section)"
-                        :disabled=" (edited_data.title === '' || edited_data.text === '') || (((edited_data.title === section.title) && (edited_data.text === section.text)) || saving)"
-                      >Save</b-button>
-                      <b-button
-                        variant="danger"
-                        @click="removeFocus(); deleteIfNew(index, section)"
-                        :disabled="saving === true"
-                      >Cancel</b-button>
-                      <b-button
-                        variant="warning"
-                        @click="showDeleteConfirmation(index)"
-                        v-show="!(section.title === '' || section.text === '') && !saving"
-                      >Delete Section</b-button>
-                      <b-spinner v-show="(saving === true)" />
-                    </b-button-toolbar>
-                  </div>
+                  <b-button
+                    :disabled="currently_edited_index !== undefined"
+                    v-show="currently_edited_index !== index"
+                    @click="addSection(index+1)"
+                  >Add section</b-button>
+                  <b-modal
+                    v-model="couldNotSave"
+                    v-b-modal.modal-center
+                    ok-only
+                    :static="true"
+                  >We couldn't save. Check your internet connection and try again. If the problem persists, contact the administrator.</b-modal>
+                  <hr v-show="index+1 !== compound.documentation.sections.length" />
                 </div>
-                <b-button
-                  :disabled="currently_edited_index !== undefined"
-                  v-show="currently_edited_index !== index"
-                  @click="addSection(index+1)"
-                >Add section</b-button>
-                <b-modal
-                  v-model="couldNotSave"
-                  v-b-modal.modal-center
-                  ok-only
-                  :static="true"
-                >We couldn't save. Check your internet connection and try again. If the problem persists, contact the administrator.</b-modal>
-                <hr v-show="index+1 !== compound.documentation.sections.length" />
               </div>
             </b-card>
           </b-col>
@@ -89,20 +96,18 @@
 </template>
 
 <script>
-
 // import ExperimentLineChart from '@/components/ExperimentLineChart.vue';
-import ReadFields from '@/components/ReadFields.vue';
-import utils from '@/views/utils';
+import ReadFields from "@/components/ReadFields.vue";
+import utils from "@/views/utils";
 
 export default {
-
   data() {
     return {
       compound: {},
       currently_edited_index: undefined,
       edited_data: {
-        title: '',
-        text: '',
+        title: "",
+        text: "",
       },
       saving: false,
       couldNotSave: false,
@@ -112,10 +117,12 @@ export default {
   },
   async mounted() {
     try {
-      const response = await this.$api.get(`${this.$compounds_url}/${this.$route.params.uid}`);
+      const response = await this.$api.get(
+        `${this.$compounds_url}/${this.$route.params.uid}`
+      );
       this.compound = response.data;
       this.mounted = true;
-      console.log('mounted');
+      console.log("mounted");
     } catch (e) {
       this.somethingWentWrong = true;
       console.log(e);
@@ -125,13 +132,16 @@ export default {
   methods: {
     async removeFocus() {
       this.currently_edited_index = undefined;
-      this.edited_data.text = '';
-      this.edited_data.title = '';
+      this.edited_data.text = "";
+      this.edited_data.title = "";
     },
     async saveEdit(indexChanged, originalSection) {
       try {
         this.saving = true;
-        this.compound.documentation.sections[indexChanged] = { title: this.edited_data.title, text: this.edited_data.text };
+        this.compound.documentation.sections[indexChanged] = {
+          title: this.edited_data.title,
+          text: this.edited_data.text,
+        };
         await this.updateDatabase();
         // TODO Change this so that you can edit the title
         this.removeFocus();
@@ -153,29 +163,33 @@ export default {
         } else {
           // Something happened in setting up the request that triggered an Error
           console.log(error);
-          console.log('Error', error.message);
+          console.log("Error", error.message);
         }
       }
     },
     async updateDatabase() {
-      const { uid } = this.compound; delete this.compound.uid;
-      await this.$api.put(`${this.$compounds_url}/${this.$route.params.uid}`, this.compound);
+      const { uid } = this.compound;
+      delete this.compound.uid;
+      await this.$api.put(
+        `${this.$compounds_url}/${this.$route.params.uid}`,
+        this.compound
+      );
       this.compound.uid = uid;
     },
     addSection(index) {
       if (!Number.isInteger(index)) {
-        throw Error('Index should be integer');
+        throw Error("Index should be integer");
       }
       const blankSection = {
-        title: '',
-        text: '',
+        title: "",
+        text: "",
       };
       this.compound.documentation.sections.splice(index, 0, blankSection);
       this.edit(index, blankSection.title, blankSection.text);
     },
     async deleteSection(index) {
       if (!Number.isInteger(index)) {
-        throw Error('Index should be integer');
+        throw Error("Index should be integer");
       }
 
       this.saving = true;
@@ -203,7 +217,7 @@ export default {
         } else {
           // Something happened in setting up the request that triggered an Error
           console.log(error);
-          console.log('Error', error.message);
+          console.log("Error", error.message);
         }
       }
     },
@@ -218,17 +232,21 @@ export default {
       }
     },
     showDeleteConfirmation(index) {
-      this.$bvModal.msgBoxConfirm('Are you sure you want to delete this section? This can\'t be undone.', {
-        title: 'Please Confirm',
-        size: 'sm',
-        buttonSize: 'sm',
-        okVariant: 'danger',
-        okTitle: 'YES',
-        cancelTitle: 'NO',
-        footerClass: 'p-2',
-        hideHeaderClose: false,
-        centered: true,
-      })
+      this.$bvModal
+        .msgBoxConfirm(
+          "Are you sure you want to delete this section? This can't be undone.",
+          {
+            title: "Please Confirm",
+            size: "sm",
+            buttonSize: "sm",
+            okVariant: "danger",
+            okTitle: "YES",
+            cancelTitle: "NO",
+            footerClass: "p-2",
+            hideHeaderClose: false,
+            centered: true,
+          }
+        )
         .then((value) => {
           if (value) {
             this.deleteSection(index);
@@ -239,7 +257,7 @@ export default {
         });
     },
     deleteIfNew(index, section) {
-      if (section.title === '' && section.text === '') {
+      if (section.title === "" && section.text === "") {
         // delete from array without leaving a hole
         this.compound.documentation.sections.splice(index, 1);
       }
@@ -253,10 +271,8 @@ export default {
     },
   },
   components: {
-    'read-fields': ReadFields,
+    "read-fields": ReadFields,
   },
-
-
 };
 </script>
 
