cbfd334c9ec421cce48df0bc803c5622ecd4cec9
thefearlesscoder <34893923+thefearlesscoder@users.noreply.github.com>
2021-03-06 17:38:56 -0800

    starting out

---
+++ src/components/editor/AdditionalReferences.vue
@@ -0,0 +1,42 @@
+<template>
+    <b-overlay :show="additionalRefsLoading" rounded="sm">
+      <b-table striped hover :items="items" :fields="table_fields">
+        <template #cell(index)="data"> {{ data.index + 1 }}. </template>
+        <template #cell(citation)="data">
+          <div align="left">
+            <a :name="data.item.doi">
+              <span
+                :class="`${
+                  $route.hash === `#${data.item.doi}` ? 'active' : ''
+                }`"
+                v-if="items[data.index].error"
+                >{{ data.value }}</span
+              >
+              <span
+                :class="`${
+                  $route.hash === `#${data.item.doi}` ? 'raw-html-active' : ''
+                }`"
+                v-else
+                v-html="data.value"
+              ></span>
+            </a>
+          </div>
+        </template>
+      </b-table>
+    </b-overlay>
+</template>
+<script>
+import AddReferences from '@/components/editor/AddReferences.vue';
+
+export default {
+  props: { refrences: Array },
+  components: {
+    AddReferences,
+  },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
